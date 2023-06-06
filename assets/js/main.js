efad9c1f42348af680d05a978b716d08c4a16e1e
bnandy05 <berenandi@gmail.com>
2023-06-06 21:39:12 +0200

    Finomgatás

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+var eversincedb=0
+var glueedb=0
+var redlightdb=0
+var exeterdb=0
+var woddb=0
+var tigerdb=0
+
 let products = [
     {
         id: 1,
@@ -28,10 +35,36 @@ let products = [
         name: 'Exeter LP',
         image: 'bladee-exeter',
         price: 9000
+    },
+    {
+        id: 6,
+        name: 'Tiger LP',
+        image: 'thaiboy-tiger',
+        price: 9000
     }
 ];
 
-function kosarba(szam)
+function kosar(szam)
 {
-    
+    switch(szam)
+    {
+        case 1:
+            eversincedb++
+            break
+        case 2:
+            glueedb++
+            break
+        case 3:
+            redlightdb++
+            break
+        case 4:
+            exeterdb++
+            break
+        case 5:
+            woddb++
+            break
+        case 6:
+            tigerdb++
+            break
+    }
 }
