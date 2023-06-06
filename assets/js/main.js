f3bcb335fc0078b605fbd749ca72c735ca2fef82
bnandy05 <berenandi@gmail.com>
2023-06-06 21:15:51 +0200

    Alapkinezet

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+let products = [
+    {
+        id: 1,
+        name: 'Eversince LP',
+        image: 'bladee-eversince',
+        price: 9000
+    },
+    {
+        id: 2,
+        name: 'Gluee LP',
+        image: 'bladee-gluee',
+        price: 9000
+    },
+    {
+        id: 3,
+        name: 'Working on Dying LP',
+        image: 'bladee-wod',
+        price: 9000
+    },
+    {
+        id: 4,
+        name: 'Red Light LP',
+        image: 'bladee-redlight',
+        price: 9000
+    },
+    {
+        id: 5,
+        name: 'Exeter LP',
+        image: 'bladee-exeter',
+        price: 9000
+    }
+];
+
+function kosarba(szam)
+{
+    
+}
